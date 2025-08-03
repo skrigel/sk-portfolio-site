178012d24b7132c4b432e7ac90df5eaedb259313
Sasha Krigel <sasha.krigel@gmail.com>
2025-08-03 23:34:17 +0100

    Adding new project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import mock02 from '../assets/images/mockdsn.png';
 import mock09 from '../assets/images/mockairi.png';
 import mock10 from '../assets/images/mockdatainf.png';
+import mock11 from '../assets/images/narrative-gen-mockup.png'
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -9,6 +10,11 @@ function Project() {
     <div className="projects-container" id="projects">
         <h1 className="page-title">Projects</h1>
         <div className="projects-grid">
+               <div className="project">
+                <a href="https://narrative-detection-simulator-1.onrender.com/" target="_blank" rel="noreferrer"><img src={mock11} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://narrative-detection-simulator-1.onrender.com/" target="_blank" rel="noreferrer"><h2>Narrative Manipulation Simulator</h2></a>
+                <p>Full-stack platform for generating and simulating the spread of ideological narratives. Includes a narrative generation playground powered by LLMs and early-stage agent-based modeling of social diffusion using React and FastAPI.</p>
+            </div>
             <div className="project">
                 <a href="https://data-infr-map.onrender.com/map/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://data-infr-map.onrender.com/map/" target="_blank" rel="noreferrer"><h2>Data Infrastructure Mapper</h2></a>
